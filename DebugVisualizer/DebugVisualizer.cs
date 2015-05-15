0c9739f06568f0eb533d825b7cedb456ebdc3f0e
Jon Fritzler <jon.fritzler@motorolasolutions.com>
2015-05-15 13:10:18 -0600

    Initial Commit :)

---
+++ DebugVisualizer/DebugVisualizer.cs
@@ -0,0 +1,24 @@
+﻿// Xml Visualizer v.2
+// by Lars Hove Christiansen (larshove@gmail.com)
+// http://www.codeplex.com/XmlVisualizer
+
+using System.IO;
+using Microsoft.VisualStudio.DebuggerVisualizers;
+
+namespace XmlVisualizer
+{
+    public class DebugVisualizer : DialogDebuggerVisualizer
+    {
+        protected override void Show(IDialogVisualizerService windowService, IVisualizerObjectProvider objectProvider)
+        {
+            string modifiedXml = Visualizer.ShowModal_LoadXmlFromString(objectProvider.GetObject().ToString(), objectProvider.IsObjectReplaceable, true);
+
+            if (Visualizer.ReplaceObject())
+            {
+                StringReader sr = new StringReader(modifiedXml);
+                objectProvider.ReplaceObject(sr.ReadToEnd());
+                sr.Close();
+            }
+        }
+    }
+}
